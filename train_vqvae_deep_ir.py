deaf07e6382afb9b189b8a8957b134cfbe862ffe
Mahdi <mahdi.alehdaghi@gmail.com>
2023-01-13 20:48:38 -0500

    Add reid only

--- train_vqvae_deep_ir.py
+++ train_vqvae_deep_ir.py
@@ -174,8 +174,8 @@ def train(epoch, loader, model, optimizer, scheduler, device, optimizer_reid):
 
             # recon_loss_feat = criterion(gray_content_itself, rgb_content_itself) +\
             #                   criterion(gray_content_other, rgb_content_itself)
-            latent_loss = latent_loss + latent_loss_ir
-            loss_G = loss_G + 0.1 * loss_Re_Ir
+
+            loss_G = loss_G + 0.1 * loss_Re_Ir + latent_loss_weight * latent_loss_ir
               # + loss_id_fake + feat_loss + loss_kl_fake
 
 
